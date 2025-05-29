e6f168dcb45e78e51b44a5a47e0b962b2146974c
thepipboy <3476647793@qq.com>
2025-05-29 11:28:16 +0800

    Update Frontface.py

--- Frontface.py
+++ Frontface.py
@@ -1,5 +1,5 @@
 import face_recognition
 
 image = face_recognition.load_image_file("group.jpg")
-face_locations = face_recognition.face_locations(image)  # 检测所有人脸位置
-face_encodings = face_recognition.face_encodings(image)  # 提取特征编码a
+face_locations = face_recognition.face_locations(image)  # detect all person position
+face_encodings = face_recognition.face_encodings(image)  # extract coding feature
